153f9591d0fa53b84d0d7a40a87fe89932ef3e4b
Samvaran Rallabandi <srallaba@redhat.com>
2022-12-04 18:54:55 -0500

    Initial export

---
+++ script.py
@@ -0,0 +1,76 @@
+from __future__ import print_function
+import httplib2
+import os
+import io
+
+from googleapiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    credential_path = os.path.join("./", 'credentials.json')
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def main():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+
+    pdffile = './punyaganamu/SH031-Punyaganamu-07.png'  # PDF file
+    txtfile = './sample.txt'  # Text file
+
+    mime = 'application/vnd.google-apps.document'
+    res = service.files().create(
+        body={
+            'name': pdffile,
+            'mimeType': mime
+        },
+        media_body=MediaFileUpload(pdffile, mimetype=mime, resumable=True)
+    ).execute()
+
+    dl = MediaIoBaseDownload(
+        io.FileIO(txtfile, 'wb'),
+        service.files().export_media(fileId=res['id'], mimeType="text/plain")
+    )
+    done = False
+    while done is False:
+        status, done = dl.next_chunk()
+    print("Done.")
+
+
+if __name__ == '__main__':
+    main()
